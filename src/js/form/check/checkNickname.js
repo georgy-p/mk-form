fc207e033e8179f6c4e8204bdf41997c1e1186d6
Georgy <pigargeorge@gmail.com>
2023-07-27 10:33:45 +0300

    add validations

--- src/js/form/check/checkNickname.js
+++ src/js/form/check/checkNickname.js
@@ -1,24 +1,81 @@
-import { isEmpty } from './isEmpty';
 import { formElements } from '../form';
-import { errors, renderErrMessage } from './controls';
+import {
+  addNextEl,
+  errors,
+  getNextSubling,
+  isEmpty,
+  isNodeP,
+} from './controls';
+import renderErrMessage from '../render/renderErrMessage';
+import { addRedBorder, removeRedBorder } from '../render/renderBorder';
+import state from '../../state';
+import checkForm from './checkForm';
+
+const nicknameRegex = /^[a-zA-Z0-9_]{3,40}$/;
+const latinsRegex = /^[a-zA-Z]/;
+const symbolsRegex = /^[a-zA-Z0-9_]*$/;
+
+const nickErrs = {
+  firstLetter: 'Никнейм должен начинаться с буквы',
+  length: 'Должно быть от 3 до 40 символов',
+  latins: 'Должны быть латинские буквы',
+  symbols: 'Нельзя использовать символы кроме цифр и нижнего подчеркивания (_)',
+};
+
+const nicknameCheck = (nickname) => {
+  const { nicknameEl } = formElements;
+
+  if (!nicknameRegex.test(nickname)) {
+    if (nickname.length < 3 || nickname.length > 40) {
+      const textEl = renderErrMessage(nickErrs.length);
+      addNextEl(nicknameEl, textEl);
+      addRedBorder(nicknameEl);
+    }
+    if (!latinsRegex.test(nickname)) {
+      const textEl = renderErrMessage(nickErrs.firstLetter);
+      addNextEl(nicknameEl, textEl);
+      addRedBorder(nicknameEl);
+    }
+    if (!symbolsRegex.test(nickname)) {
+      const textEl = renderErrMessage(nickErrs.symbols);
+      addNextEl(nicknameEl, textEl);
+      addRedBorder(nicknameEl);
+    }
+  } else {
+    const pEl = getNextSubling(nicknameEl);
+    if (isNodeP(pEl)) {
+      removeRedBorder(nicknameEl);
+      pEl.remove();
+    }
+  }
+};
 
 export default () => {
-  const { nickname } = formElements;
+  const { nicknameEl } = formElements;
 
-  nickname.addEventListener('input', () => {
-    const pEl = nickname.nextElementSibling;
-    if (pEl && pEl.nodeName === 'P') {
+  nicknameEl.addEventListener('input', ({ target }) => {
+    const { value } = target;
+    state.form.nickname = value;
+    checkForm();
+    const pEl = getNextSubling(nicknameEl);
+    if (isNodeP(pEl)) {
+      removeRedBorder(nicknameEl);
       pEl.remove();
+    }
+
+    if (!isEmpty(value)) {
+      nicknameCheck(value);
     }
   });
 
-  nickname.addEventListener('blur', ({ target }) => {
+  nicknameEl.addEventListener('blur', ({ target }) => {
     const { value } = target;
     if (isEmpty(value)) {
-      const nextEl = nickname.nextElementSibling;
-      if (nextEl.nodeName !== 'P') {
+      const nextEl = getNextSubling(nicknameEl);
+      addRedBorder(nicknameEl);
+      if (!isNodeP(nextEl)) {
         const textEl = renderErrMessage(errors.emptyErr);
-        nickname.insertAdjacentElement('afterend', textEl);
+        addNextEl(nicknameEl, textEl);
       }
     }
   });
